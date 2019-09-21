6f189fee45590a46432ab03885c866fb3262b889
Lucas Severino <lucas.severino@itlean.com.br>
2019-09-21 19:49:39 -0300

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { AppRoutingModule } from './app.routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotosModule } from './photos/photos.module';
+import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PhotosModule,
+    AppRoutingModule,
+    ErrorsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoService } from './photo/photo.service';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+
+@NgModule({
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [PhotoService]
+})
+
+export class PhotosModule {
+
+}
