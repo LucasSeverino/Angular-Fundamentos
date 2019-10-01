3151228937f238abeca4bf03d804a6d1ad700d2a
Lucas dos Santos Severino <lucasseverino@MacBook-Pro-de-Lucas.local>
2019-09-30 22:53:56 -0300

    aula 3 - Curso Angular parte 3

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
-import { PhotoFormComponent } from "./photo-form.component";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+
+import { PhotoFormComponent } from "./photo-form.component";
 import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { PhotoModule } from "../photo/photo.module";
+import { ImmediateClickModule } from "../../shared/directives/immediate-click/immediate-click.module";
 
 @NgModule({
     declarations: [
@@ -14,7 +17,9 @@ import { RouterModule } from "@angular/router";
         FormsModule,
         RouterModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        PhotoModule,
+        ImmediateClickModule
     ]
 })
 
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,13 +4,15 @@ import { PhotoService } from './photo/photo.service';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoModule } from './photo/photo.module';
+import { PhotoDetailsModule } from './photo-details/photo-details.module';
 
 @NgModule({
   declarations: [],
   imports: [
     PhotoModule,
     PhotoFormModule,
-    PhotoListModule
+    PhotoListModule,
+    PhotoDetailsModule
   ],
   providers: [PhotoService]
 })
