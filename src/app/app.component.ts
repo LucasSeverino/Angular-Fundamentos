92e25ec4e85f5f3f8f5d9a0fe9634db65e22037d
Lucas dos Santos Severino <lucasseverino@MacBook-Pro-de-Lucas.local>
2019-10-02 01:04:18 -0300

    aula 1 - Curso Angular parte 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) { }
+
+  ngOnInit() {
+    this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      switchMap(route => route.data)
+    )
+    .subscribe(event => {
+      this.titleService.setTitle(event.title);
+    })
+  }
+}
